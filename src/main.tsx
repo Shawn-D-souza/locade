8a116fc771772755290e4362c5dfeaf116ccaa4b
Shawn Dsouza <shawndsouz@gmail.com>
2026-07-14 12:29:36 +0530

    feat: integrate PostHog analytics

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,23 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { PostHogProvider } from '@posthog/react'
+
+const options = {
+  api_host: import.meta.env.VITE_POSTHOG_HOST,
+  disable_session_recording: true,
+  errorTracking: {
+    autocapture: {
+      uncaughtExceptions: true,
+      unhandledRejections: true,
+    },
+  },
+}
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <PostHogProvider apiKey={import.meta.env.VITE_POSTHOG_PROJECT_TOKEN} options={options}>
+      <App />
+    </PostHogProvider>
   </StrictMode>,
 )
